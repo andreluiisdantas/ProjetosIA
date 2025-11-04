e9c8ed075e80f5a9e44dc907c05591af04668dbd
andreluiisdantas <andreluissousaa10@gmail.com>
2025-11-04 10:46:27 -0300

    Definindo llm

--- Aula01-Arquitetura-RAG/main.py
+++ Aula01-Arquitetura-RAG/main.py
@@ -1,3 +1,11 @@
 import os
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import ChatPromptTemplate
 
+# Defining API Key
 os.environ["GOOGLE_API_KEY"] = "AIzaSyCc1XN7WzYTqY5dtGO4bOgRDn2sk5O8BKs"
+
+# Defining llm
+llm = ChatGoogleGenerativeAI(model = "gemini-1.5-pro-latest", temperature = 0)
+
+print(llm)
